e2d3aa4e20b197839fcc773ca11bd71417d7f5bf
hafsah <g00089492@aus.edu>
2024-01-31 15:09:03 +0400

    Feature b added

--- src/software1/coe420.java
+++ src/software1/coe420.java
@@ -5,6 +5,7 @@ public class coe420 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		System.out.println("change by member b :)");
 	}
 
 }
